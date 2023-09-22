c530f805297985256ee094cf8b393e05a680b85b
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2023-09-22 07:42:29 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(cors()) ;
+app.use(express.json());
+
+
+
+app.get('/', function (req, res) {
+  res.json({message: " Welcome to contact book applications!!"});
+})
+
+module.exports = app;
