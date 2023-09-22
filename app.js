d17a73515ab2713bcff261c38c7b5788e1a6d939
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2023-09-22 08:06:40 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/route");
 const app = express();
 
@@ -7,7 +8,15 @@ const app = express();
 app.use(cors()) ;
 app.use(express.json());
 
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
 
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 app.get('/', function (req, res) {
   res.json({message: " Welcome to contact book applications!!"});
